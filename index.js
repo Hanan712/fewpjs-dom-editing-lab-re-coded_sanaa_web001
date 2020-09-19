8fa5f7cb42165ac39e46596702fc47a4c1449a5c
hanan7121993 <hanan7121993@gmail.com>
2020-09-19 15:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+const main = document.getElementById("main");
+main.remove()
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = null;
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerText = "Angela is the champion!";
+document.body.appendChild(newHeader);
